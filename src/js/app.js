9dcc91356fd9444edbb7e04679de187f1c22fb2f
Steven Coaila Zaa <scoailazaa@gmail.com>
2020-03-17 16:46:22 -0500

    vue.js card example

--- src/js/app.js
+++ src/js/app.js
@@ -11,11 +11,25 @@ fetch('http://localhost:3000/videos')
 function createCard(video) {
   const {title, url_video, description} = video;
   const card = document.createElement('article');
+  card.classList.add('card-video');
 
   card.innerHTML = `
-    <h2> ${title} </h2>
-    <h3> ${url_video} </h3>
-    <h4> ${description} </h4>
+  <figure class="card-video__preview-container">
+    <button class="card-video__edit" id="js_editVideo"><i class="fas fa-pencil-alt"></i></button>
+    <button class="card-video__delete" id="js_deleteVideo"><i class="fas fa-times"></i></button>
+    <img class="card-video__preview" src="${url_video}" alt="Img">
+  </figure>
+
+  <div class="card-video__container-detail">
+    <h3 class="card-video__sub-title">${title}</h3>
+    <p class="card-video__views">0 visualizaciones</p>
+
+    <p class="card-video__description">
+      ${description}
+    </p>
+
+    <button class="btn btn--detail" id="js_btnViewDetail">Ver detalle</button>
+  </div>
   `
 
   return card;
